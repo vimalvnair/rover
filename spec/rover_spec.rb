e8dec8d4a9462f4cc7a981e77fdb10b4f10d7153
Vimal V Nair <vimalvnair999@gmail.com>
2013-04-23 23:34:34 +0530

    First commit

---
+++ spec/rover_spec.rb
@@ -0,0 +1,87 @@
+require_relative "../src/rover.rb"
+
+describe Rover do
+  describe "Creation" do
+    it "should be at origin on when initialized" do
+      rover = Rover.new
+      rover.position_x.should eql(0)
+      rover.position_y.should eql(0)
+      rover.direction.should eql('N')
+    end
+    
+    it "should have the position passed on creation" do
+      rover = Rover.new(2, 3, 'S')
+      rover.position_x.should eql(2)
+      rover.position_y.should eql(3)
+      rover.direction.should eql('S')
+    end
+  end
+
+  describe "Move" do
+    it "should update its postion to the given displacement" do
+      rover = Rover.new(1, 5)
+      rover.move(1, 2)
+      rover.position_x.should eql(1)
+      rover.position_y.should eql(7)      
+      rover.move(8, -2)
+      rover.position_x.should eql(1)
+      rover.position_y.should eql(5)      
+    end
+  end
+
+  describe "Rotate" do
+    it "should turn to left" do
+      rover = Rover.new(1, 5, 'S')
+      rover.rotate('L')
+      rover.direction.should eql('E')
+      rover.rotate('L')
+      rover.direction.should eql('N')
+      rover.rotate('L')
+      rover.direction.should eql('W')
+      rover.rotate('L')
+      rover.direction.should eql('S')
+    end
+
+    it "should turn to left" do
+      rover = Rover.new(1, 5, 'N')
+      rover.rotate('R')
+      rover.direction.should eql('E')
+      rover.rotate('R')
+      rover.direction.should eql('S')
+      rover.rotate('R')
+      rover.direction.should eql('W')
+      rover.rotate('R')
+      rover.direction.should eql('N')
+    end
+
+    it "should turn left and right" do
+      rover = Rover.new(1, 5, 'N')
+      rover.rotate('R')
+      rover.direction.should eql('E')
+      rover.rotate('L')
+      rover.direction.should eql('N')
+    end
+  end
+
+  describe "Rotate and move" do
+    it "should turn and move" do
+      rover = Rover.new(3, 5, 'W')
+      rover.rotate('L')
+      rover.move(5, 4)
+      rover.position_x.should eql(3)
+      rover.position_y.should eql(1)      
+    end
+  end
+  describe "Control signal" do
+    it "should respond to control signal" do 
+      rover = Rover.new(3, 5, 'N')
+      rover.control_signal("L")
+      rover.control_signal("M")
+      rover.control_signal("L")
+      rover.control_signal("M")
+      rover.position_x.should eql(2)
+      rover.position_y.should eql(4)
+      rover.direction.should eql("S")
+    end
+  end
+end
---
+++ src/rover.rb
@@ -0,0 +1,62 @@
+class Rover
+  TURN = {"L" => -1, "R" => 1}
+  DIRECTION = {"N" => 0, "E" => 1, "S" => 2, "W" => 3}
+  DIRECTION_COORDINATES = {
+    "N" => [0, 1], 
+    "E" => [1, 0], 
+    "S" => [0, -1], 
+    "W" => [-1, 0]
+  }
+
+  attr_accessor :position_x, :position_y
+  attr_accessor :direction
+  
+  def initialize(position_x = 0, position_y = 0, direction = 'N')
+    @position_x, @position_y = position_x, position_y
+    @direction = direction
+    set_direction_cordinates
+  end
+  
+  def move(displacement_x, displacement_y)
+    set_direction_cordinates
+    @position_x = @position_x + displacement_x * @direction_x
+    @position_y = @position_y + displacement_y * @direction_y
+  end
+  
+  def rotate(turn)
+    current_direction_value = DIRECTION[@direction]
+    turn_value = TURN[turn]
+    @new_direction_value = current_direction_value + turn_value
+    check_boundaries
+    @direction = DIRECTION.key(@new_direction_value)
+  end
+
+  def control_signal(command)
+    rotate(command) and return if TURN.keys.include?(command)
+    move(1, 1) and return if command == "M"
+  end
+
+  def check_boundaries
+    @new_direction_value = 3 if @new_direction_value < 0
+    @new_direction_value = 0 if @new_direction_value > 3
+  end
+
+  def set_direction_cordinates
+    @direction_x = DIRECTION_COORDINATES[@direction][0]
+    @direction_y = DIRECTION_COORDINATES[@direction][1]
+  end
+end
+
+##Run
+
+curiosity = Rover.new(1, 2, 'N')
+['L','M','L','M','L','M','L','M','M'].each do |command|
+  curiosity.control_signal(command)
+end
+puts "Position = #{curiosity.position_x}, #{curiosity.position_y} #{curiosity.direction}"
+
+opportunity = Rover.new(3, 3, 'E')
+['M','M','R','M','M','R','M','R','R', 'M'].each do |command|
+  opportunity.control_signal(command)
+end
+puts "Position = #{opportunity.position_x}, #{opportunity.position_y} #{opportunity.direction}"
